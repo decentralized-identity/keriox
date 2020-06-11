69e7356ce227534acf09a5d69c04d73659c2b6f4
Charles Cunningham <c.a.cunningham6@gmail.com>
2020-06-11 22:35:22 +0200

    feat(prefix): padding util fns

--- src/prefix/mod.rs
+++ src/prefix/mod.rs
@@ -46,3 +46,26 @@ fn parse_padded(padded_code: &str) -> Result<(Derivation, usize), Error> {
         _ => Ok((Derivation::from_str(&head)?, 1)),
     }
 }
+
+// TODO in future, derivation codes may have >1 char representations,
+// number of 0s will become dependant on derivation code value as well
+fn pad_to_length(derivation_code: &Derivation, len: usize) -> String {
+    let prefix = match len % 4 {
+        3 => "10",
+        2 => "0",
+        1 => "",
+        0 => "200",
+        // HACK should NEVER happen given how % works
+        _ => "",
+    };
+
+    [prefix, &derivation_code.to_str()].join("")
+}
+
+/// Counts the number of padding bytes (=) in a base64 encoded string,
+/// based on which returns the size we should use for the prefix bytes.
+/// Section 14. Derivation Codes.
+
+pub fn get_prefix_length(value: &str) -> usize {
+    return value.matches('=').count();
+}
