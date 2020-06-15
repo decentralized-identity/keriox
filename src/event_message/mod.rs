60efd6af2bc7a83b2fc3fc4bbe06ba4be98e2fc0
Charles Cunningham <c.a.cunningham6@gmail.com>
2020-06-15 20:30:52 +0200

    feat(event_message): event message struct

---
+++ src/event_message/mod.rs
@@ -0,0 +1,22 @@
+use crate::events::Event;
+use serde::{Deserialize, Serialize};
+
+/// Event Message
+///
+/// An EventMessage represents any signed message involved in the KERI protocol
+/// All types share mandatory prefix, sn, sig config and signatures fields, but differ
+/// in the event data they carry
+#[derive(Serialize, Deserialize)]
+pub struct EventMessage {
+    #[serde(flatten)]
+    pub event: Event,
+
+    #[serde(rename(serialize = "sigs", deserialize = "sigs"))]
+    pub sig_config: Vec<u64>,
+
+    /// Appended Signatures
+    ///
+    /// TODO in the recommended JSON encoding, the signatures are appended to the json body.
+    /// how do you do that in serde?
+    pub signatures: Vec<Prefix>,
+}
