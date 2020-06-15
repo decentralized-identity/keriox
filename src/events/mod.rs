3dd2ed176b09fa4ffcd50989393d173b1206ec62
Charles Cunningham <c.a.cunningham6@gmail.com>
2020-06-15 19:54:49 +0200

    feat(events): add Event struct & EventData enum

--- src/events/mod.rs
+++ src/events/mod.rs
@@ -1,1 +1,33 @@
 use crate::prefix::Prefix;
+use serde::{Deserialize, Serialize};
+pub mod event_data;
+pub mod sections;
+
+use self::event_data::{
+    delegated::{DelegatedInceptionEvent, DelegatedRotationEvent},
+    inception::InceptionEvent,
+    interaction::InteractionEvent,
+    receipt::EventReceipt,
+    rotation::RotationEvent,
+};
+
+#[derive(Serialize, Deserialize)]
+pub struct Event {
+    #[serde(rename(serialize = "id", deserialize = "id"))]
+    pub prefix: Prefix,
+
+    pub sn: u64,
+
+    #[serde(flatten)]
+    pub event_data: EventData,
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum EventData {
+    Icp(InceptionEvent),
+    Rot(RotationEvent),
+    Ixn(InteractionEvent),
+    Dip(DelegatedInceptionEvent),
+    Drt(DelegatedRotationEvent),
+    Rct(EventReceipt),
+}
