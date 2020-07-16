75647b5b7b8d9d2d8ab91e7f65dea4c3e488affe
Exulansis <rusu.eugeniu@gmx.de>
2020-07-16 11:23:57 +0200

    Removed WASM dependency / macros

--- src/derivation/mod.rs
+++ src/derivation/mod.rs
@@ -4,13 +4,11 @@ use ursa::hash::{
     sha3::{Sha3_256, Sha3_512},
     Digest,
 };
-use wasm_bindgen::prelude::*;
 
 /// Basic Derivations
 ///
 /// Basic prefix derivation is just a public key (2.3.1)
 ///
-#[wasm_bindgen]
 pub fn basic_key_derivation(key: &[u8]) -> Vec<u8> {
     key.to_vec()
 }
@@ -19,7 +17,6 @@ pub fn basic_key_derivation(key: &[u8]) -> Vec<u8> {
 ///
 /// A self signing prefix derivation outputs a signature as its derivative (2.3.5)
 ///
-#[wasm_bindgen]
 pub fn self_signing_derivation(sig: &[u8]) -> Vec<u8> {
     sig.to_vec()
 }
@@ -30,53 +27,44 @@ pub fn self_signing_derivation(sig: &[u8]) -> Vec<u8> {
 ///   Multi-sig Self-addressing is a self-addressing where the inception data is the public key info of the multisig set (2.3.3)
 ///   Delegated Self-addressing uses the Dip event data for the inception data (2.3.4)
 ///
-#[wasm_bindgen]
 pub fn blake3_256_digest(_input: &[u8]) -> Vec<u8> {
     todo!()
 }
 
-#[wasm_bindgen]
 pub fn blake2s_256_digest(_input: &[u8]) -> Vec<u8> {
     todo!()
 }
 
-#[wasm_bindgen]
 pub fn blake2b_256_digest(input: &[u8]) -> Vec<u8> {
     Blake2::digest(input).to_vec()
 }
 
-#[wasm_bindgen]
 pub fn blake3_512_digest(_input: &[u8]) -> Vec<u8> {
     todo!()
 }
 
-#[wasm_bindgen]
 pub fn blake2b_512_digest(_input: &[u8]) -> Vec<u8> {
     todo!()
 }
 
-#[wasm_bindgen]
 pub fn sha3_256_digest(input: &[u8]) -> Vec<u8> {
     let mut h = Sha3_256::new();
     h.input(input);
     h.result().to_vec()
 }
 
-#[wasm_bindgen]
 pub fn sha2_256_digest(input: &[u8]) -> Vec<u8> {
     let mut h = Sha256::new();
     h.input(input);
     h.result().to_vec()
 }
 
-#[wasm_bindgen]
 pub fn sha3_512_digest(input: &[u8]) -> Vec<u8> {
     let mut h = Sha3_512::new();
     h.input(input);
     h.result().to_vec()
 }
 
-#[wasm_bindgen]
 pub fn sha2_512_digest(input: &[u8]) -> Vec<u8> {
     let mut h = Sha512::new();
     h.input(input);
