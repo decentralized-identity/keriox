d905daf4dfdf2817160f41678048d815b0ecf97f
Charles Cunningham <c.a.cunningham6@gmail.com>
2020-10-15 16:41:17 +0200

    doc(rct): add some context doc to receipt types

--- src/event/event_data/receipt.rs
+++ src/event/event_data/receipt.rs
@@ -3,16 +3,23 @@ use crate::prefix::SelfAddressingPrefix;
 use crate::state::EventSemantics;
 use serde::{Deserialize, Serialize};
 
+/// Non-Transferrable Receipt
+///
+/// A receipt created by an Identifier of a non-transferrable type.
+/// Mostly intended for use by Witnesses.
+/// NOTE: This receipt has a unique structure to it's appended
+/// signatures
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct ReceiptNonTransferable {}
 
 impl EventSemantics for ReceiptNonTransferable {}
 
-/// Validator Receipt
+/// Transferrable Receipt
 ///
 /// Event Receipt which is suitable for creation by Transferable
 /// Identifiers. Provides both the signatures and a commitment to
-/// the latest establishment event of the Validator.
+/// the latest establishment event of the receipt creator.
+/// Mostly intended for use by Validators
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct ReceiptTransferable {
     /// Receipted Event Digest
