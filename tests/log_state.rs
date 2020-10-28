724c8ad89d05f905dc39fafd1278cfa14a6b6d62
Edyta Pawlak <edyta.pawlakk@gmail.com>
2020-10-28 20:23:56 +0100

    test(dm): add direct mode test

---
+++ tests/log_state.rs
@@ -0,0 +1,302 @@
+use keri::{
+    derivation::{basic::Basic, self_addressing::SelfAddressing, self_signing::SelfSigning},
+    error::Error,
+    event::{
+        event_data::{
+            inception::InceptionEvent, interaction::InteractionEvent, receipt::ReceiptTransferable,
+            rotation::RotationEvent, EventData,
+        },
+        sections::seal::DigestSeal,
+        sections::{seal::EventSeal, seal::Seal, InceptionWitnessConfig, KeyConfig, WitnessConfig},
+        Event,
+    },
+    event_message::{
+        parse::signed_event_stream, serialization_info::SerializationFormats, EventMessage,
+        SignedEventMessage,
+    },
+    prefix::{AttachedSignaturePrefix, IdentifierPrefix, Prefix},
+    state::IdentifierState,
+    util::dfs_serializer,
+};
+use serde_json::to_string_pretty;
+use std::{collections::HashMap, str::from_utf8};
+use ursa::{
+    keys::{PrivateKey, PublicKey},
+    signatures::{ed25519, SignatureScheme},
+};
+
+pub struct LogState {
+    pub log: Vec<SignedEventMessage>,
+    pub state: IdentifierState,
+    pub receipts: HashMap<u64, Vec<SignedEventMessage>>,
+    pub escrow_sigs: Vec<SignedEventMessage>,
+    pub keypair: (PublicKey, PrivateKey),
+    pub next_keypair: (PublicKey, PrivateKey),
+    pub other_instances: HashMap<String, IdentifierState>,
+}
+impl LogState {
+    // incept a state and keys
+    pub fn new() -> Result<LogState, Error> {
+        let ed = ed25519::Ed25519Sha512::new();
+        let keypair = ed
+            .keypair(Option::None)
+            .map_err(|e| Error::CryptoError(e))?;
+        let next_keypair = ed
+            .keypair(Option::None)
+            .map_err(|e| Error::CryptoError(e))?;
+
+        let icp_data = InceptionEvent {
+            key_config: KeyConfig {
+                threshold: 1,
+                public_keys: vec![Basic::Ed25519.derive(keypair.0.clone())],
+                threshold_key_digest: SelfAddressing::Blake3_256.derive(
+                    Basic::Ed25519
+                        .derive(next_keypair.0.clone())
+                        .to_str()
+                        .as_bytes(),
+                ),
+            },
+            witness_config: InceptionWitnessConfig::default(),
+            inception_configuration: vec![],
+        };
+
+        let icp_data_message = EventMessage::get_inception_data(
+            &icp_data,
+            SelfAddressing::Blake3_256,
+            &SerializationFormats::JSON,
+        );
+
+        let pref = IdentifierPrefix::SelfAddressing(
+            SelfAddressing::Blake3_256.derive(&dfs_serializer::to_vec(&icp_data_message)?),
+        );
+
+        let icp_m = Event {
+            prefix: pref.clone(),
+            sn: 0,
+            event_data: EventData::Icp(icp_data),
+        }
+        .to_message(&SerializationFormats::JSON)?;
+
+        let ed = ed25519::Ed25519Sha512::new();
+        let sigged = icp_m.sign(vec![AttachedSignaturePrefix::new(
+            SelfSigning::Ed25519Sha512,
+            ed.sign(&icp_m.serialize()?, &keypair.1)
+                .map_err(|e| Error::CryptoError(e))?,
+            0,
+        )]);
+
+        let s0 = IdentifierState::default().verify_and_apply(&sigged)?;
+
+        Ok(LogState {
+            log: vec![sigged],
+            receipts: HashMap::new(),
+            state: s0,
+            keypair,
+            next_keypair,
+            escrow_sigs: vec![],
+            other_instances: HashMap::new(),
+        })
+    }
+
+    // take a receipt made by validator, verify it and add to receipts or escrow
+    pub fn add_sig(
+        &mut self,
+        validator: &IdentifierState,
+        sigs: SignedEventMessage,
+    ) -> Result<(), Error> {
+        match sigs.event_message.event.event_data.clone() {
+            EventData::Vrc(rct) => {
+                let event = self
+                    .log
+                    .get(sigs.event_message.event.sn as usize)
+                    .ok_or(Error::SemanticError("incorrect receipt sn".into()))?;
+
+                // This logic can in future be moved to the correct place in the Kever equivalent here
+                // receipt pref is the ID who made the event being receipted
+                if sigs.event_message.event.prefix == self.state.prefix
+                            // dig is the digest of the event being receipted
+                            && rct.receipted_event_digest
+                                == rct
+                                    .receipted_event_digest
+                                    .derivation
+                                    .derive(&event.event_message.serialize()?)
+                            // seal pref is the pref of the validator
+                            && rct.validator_location_seal.prefix == validator.prefix
+                {
+                    if rct.validator_location_seal.event_digest
+                        == rct
+                            .validator_location_seal
+                            .event_digest
+                            .derivation
+                            .derive(&validator.last)
+                    {
+                        // seal dig is the digest of the last establishment event for the validator, verify the rct
+                        validator.verify(&event.event_message.sign(sigs.signatures.clone()))?;
+                        self.receipts
+                            .entry(sigs.event_message.event.sn)
+                            .or_insert_with(|| vec![])
+                            .push(sigs);
+                    } else {
+                        // escrow the seal
+                        self.escrow_sigs.push(sigs)
+                    }
+                    Ok(())
+                } else {
+                    Err(Error::SemanticError("incorrect receipt binding".into()))
+                }
+            }
+            _ => Err(Error::SemanticError("not a receipt".into())),
+        }
+    }
+
+    pub fn make_rct(&self, event: EventMessage) -> Result<SignedEventMessage, Error> {
+        let ser = event.serialize()?;
+        Ok(Event {
+            prefix: event.event.prefix,
+            sn: event.event.sn,
+            event_data: EventData::Vrc(ReceiptTransferable {
+                receipted_event_digest: SelfAddressing::Blake3_256.derive(&ser),
+                validator_location_seal: EventSeal {
+                    prefix: self.state.prefix.clone(),
+                    event_digest: SelfAddressing::Blake3_256.derive(&self.state.last),
+                },
+            }),
+        }
+        .to_message(&SerializationFormats::JSON)?
+        .sign(vec![AttachedSignaturePrefix::new(
+            SelfSigning::Ed25519Sha512,
+            ed25519::Ed25519Sha512::new()
+                .sign(&ser, &self.keypair.1)
+                .map_err(|e| Error::CryptoError(e))?,
+            0,
+        )]))
+    }
+
+    pub fn rotate(&mut self) -> Result<SignedEventMessage, Error> {
+        let ed = ed25519::Ed25519Sha512::new();
+        let keypair = self.next_keypair.clone();
+        let next_keypair = ed
+            .keypair(Option::None)
+            .map_err(|e| Error::CryptoError(e))?;
+
+        let ev = Event {
+            prefix: self.state.prefix.clone(),
+            sn: self.state.sn + 1,
+            event_data: EventData::Rot(RotationEvent {
+                previous_event_hash: SelfAddressing::Blake3_256.derive(&self.state.last),
+                key_config: KeyConfig {
+                    threshold: 1,
+                    public_keys: vec![Basic::Ed25519.derive(keypair.0.clone())],
+                    threshold_key_digest: SelfAddressing::Blake3_256.derive(
+                        Basic::Ed25519
+                            .derive(next_keypair.0.clone())
+                            .to_str()
+                            .as_bytes(),
+                    ),
+                },
+                witness_config: WitnessConfig::default(),
+                data: vec![],
+            }),
+        }
+        .to_message(&SerializationFormats::JSON)?;
+
+        let rot = ev.sign(vec![AttachedSignaturePrefix::new(
+            SelfSigning::Ed25519Sha512,
+            ed25519::Ed25519Sha512::new()
+                .sign(&ev.serialize()?, &keypair.1)
+                .map_err(|e| Error::CryptoError(e))?,
+            0,
+        )]);
+
+        self.state = self.state.clone().verify_and_apply(&rot)?;
+
+        self.log.push(rot.clone());
+
+        self.keypair = keypair;
+        self.next_keypair = next_keypair;
+
+        Ok(rot)
+    }
+
+    pub fn make_ixn(&mut self, payload: &str) -> Result<SignedEventMessage, Error> {
+        let dig_seal = DigestSeal {
+            dig: SelfAddressing::Blake3_256.derive(payload.as_bytes()),
+        };
+
+        let ev = Event {
+            prefix: self.state.prefix.clone(),
+            sn: self.state.sn + 1,
+            event_data: EventData::Ixn(InteractionEvent {
+                previous_event_hash: SelfAddressing::Blake3_256.derive(&self.state.last),
+                data: vec![Seal::Digest(dig_seal)],
+            }),
+        }
+        .to_message(&SerializationFormats::JSON)?;
+
+        let ixn = ev.sign(vec![AttachedSignaturePrefix::new(
+            SelfSigning::Ed25519Sha512,
+            ed25519::Ed25519Sha512::new()
+                .sign(&ev.serialize().unwrap(), &self.keypair.1)
+                .map_err(|e| Error::CryptoError(e))?,
+            0,
+        )]);
+
+        self.state = self.state.clone().verify_and_apply(&ixn)?;
+        self.log.push(ixn.clone());
+        Ok(ixn)
+    }
+
+    pub fn process_events(&mut self, msg: Vec<u8>) -> Vec<SignedEventMessage> {
+        let events = signed_event_stream(from_utf8(&msg).unwrap()).unwrap().1;
+        let mut response: Vec<SignedEventMessage> = vec![];
+        for ev in events {
+            match ev.event_message.event.event_data {
+                EventData::Vrc(ref vrc) => {
+                    println!(
+                        "------\n{} received receipt:\n{}",
+                        self.state.prefix.to_str(),
+                        from_utf8(&ev.serialize().unwrap()).unwrap()
+                    );
+                    let prefix_str = vrc.validator_location_seal.prefix.to_str();
+                    let validator = &self.other_instances.get(&prefix_str).unwrap().clone();
+                    self.add_sig(validator, ev).unwrap();
+                    println!(
+                        "------\nnew local state: {}\n",
+                        to_string_pretty(&self.state).unwrap()
+                    );
+                }
+                _ => {
+                    println!(
+                        "------\n{} received event:\n{}",
+                        self.state.prefix.to_str(),
+                        from_utf8(&ev.serialize().unwrap()).unwrap()
+                    );
+
+                    let prefix_str = ev.event_message.event.prefix.to_str();
+
+                    let state = self
+                        .other_instances
+                        .remove(&prefix_str)
+                        .unwrap_or(IdentifierState::default());
+                    self.other_instances
+                        .insert(prefix_str.clone(), state.verify_and_apply(&ev).unwrap());
+
+                    println!(
+                        "------\nnew remote state: {}\n",
+                        to_string_pretty(&self.other_instances.get(&prefix_str)).unwrap()
+                    );
+                    // send receipt
+                    let rct = self.make_rct(ev.event_message).unwrap();
+                    let rct_s = rct.serialize().unwrap();
+                    println!(
+                        "------\n{} sending receipt: \n{}",
+                        self.state.prefix.to_str(),
+                        from_utf8(&rct_s).unwrap()
+                    );
+                    response.push(rct);
+                }
+            }
+        }
+        response
+    }
+}
---
+++ tests/test_direct_mode.rs
@@ -0,0 +1,143 @@
+mod log_state;
+use keri::{error::Error, event::event_data::EventData, prefix::Prefix};
+use log_state::LogState;
+
+#[test]
+fn test_direct_mode() -> Result<(), Error> {
+    // Init alice.
+    let mut alice = LogState::new()?;
+    assert_eq!(alice.state.sn, 0);
+    assert_eq!(alice.log.len(), 1);
+    assert!(matches!(
+        alice.log.first().unwrap().event_message.event.event_data,
+        EventData::Icp(_)
+    ));
+    assert!(alice.other_instances.is_empty());
+    assert!(alice.receipts.is_empty());
+    assert!(alice.escrow_sigs.is_empty());
+
+    // Init bob.
+    let mut bob = LogState::new()?;
+    assert_eq!(bob.state.sn, 0);
+    assert_eq!(bob.log.len(), 1);
+    assert!(matches!(
+        bob.log.first().unwrap().event_message.event.event_data,
+        EventData::Icp(_)
+    ));
+    assert!(bob.other_instances.is_empty());
+    assert!(bob.receipts.is_empty());
+    assert!(bob.escrow_sigs.is_empty());
+
+    // Serialize alice inception event.
+    let mut msg_to_bob = alice.log.last().unwrap().serialize()?;
+
+    // Simulate sending it to bob.
+    let mut bob_receipts = bob.process_events(msg_to_bob);
+
+    {
+        // Check if bob's state of alice is the same as current alice state.
+        let alice_state_in_bob = bob
+            .other_instances
+            .get(&alice.state.prefix.to_str())
+            .unwrap();
+        assert_eq!(*alice_state_in_bob, alice.state);
+    }
+
+    // Prepare message from bob to alice with bob's inception event and receipt.
+    let mut receipts = bob_receipts
+        .iter()
+        .map(|x| x.serialize())
+        .filter_map(Result::ok)
+        .collect::<Vec<_>>()
+        .concat();
+    let mut msg_to_alice = bob.log.last().unwrap().serialize().unwrap();
+    msg_to_alice.append(&mut receipts);
+
+    // Send message from bob to alice and get alice's receipts.
+    let alice_receipts = alice.process_events(msg_to_alice);
+    assert_eq!(alice_receipts.len(), 1);
+
+    {
+        // Check if alice's state of bob is the same as current bob state.
+        let bob_state_in_alice = alice
+            .other_instances
+            .get(&bob.state.prefix.to_str())
+            .unwrap();
+        assert_eq!(*bob_state_in_alice, bob.state);
+        assert_eq!(alice.receipts.len(), 1);
+    }
+
+    // Prepare receipt message for bob.
+    msg_to_bob = alice_receipts
+        .iter()
+        .map(|x| x.serialize())
+        .filter_map(Result::ok)
+        .collect::<Vec<_>>()
+        .concat();
+
+    // Send it to bob.
+    bob.process_events(msg_to_bob);
+    assert_eq!(bob.receipts.len(), 1);
+
+    // Rotation event.
+    alice.rotate()?;
+    assert_eq!(alice.log.len(), 2);
+    assert_eq!(alice.state.sn, 1);
+
+    // Send rotation event to bob.
+    msg_to_bob = alice.log.last().unwrap().serialize()?;
+    bob_receipts = bob.process_events(msg_to_bob);
+    {
+        // Check if bob's state of alice is the same as current alice state.
+        let alice_state_in_bob = bob
+            .other_instances
+            .get(&alice.state.prefix.to_str())
+            .unwrap();
+        assert_eq!(*alice_state_in_bob, alice.state);
+        assert_eq!(alice.state.sn, 1);
+        assert_eq!(alice.log.len(), 2);
+    }
+    msg_to_alice = bob_receipts
+        .iter()
+        .map(|x| x.serialize())
+        .filter_map(Result::ok)
+        .collect::<Vec<_>>()
+        .concat();
+
+    // Send bob's receipt to alice.
+    alice.process_events(msg_to_alice);
+    assert_eq!(alice.receipts.len(), 2);
+    assert_eq!(alice.escrow_sigs.len(), 0);
+
+    // Interaction event.
+    alice.make_ixn("")?;
+    assert_eq!(alice.log.len(), 3);
+    assert_eq!(alice.state.sn, 2);
+
+    // Send interaction event to bob.
+    msg_to_bob = alice.log.last().unwrap().serialize()?;
+    bob_receipts = bob.process_events(msg_to_bob);
+
+    {
+        // Check if bob's state of alice is the same as current alice state.
+        let alice_state_in_bob = bob
+            .other_instances
+            .get(&alice.state.prefix.to_str())
+            .unwrap();
+        assert_eq!(*alice_state_in_bob, alice.state);
+    }
+
+    // Send bob's receipt to alice.
+    msg_to_alice = bob_receipts
+        .iter()
+        .map(|x| x.serialize())
+        .filter_map(Result::ok)
+        .collect::<Vec<_>>()
+        .concat();
+
+    alice.process_events(msg_to_alice);
+    assert_eq!(alice.receipts.len(), 3);
+    assert_eq!(alice.escrow_sigs.len(), 0);
+
+    Ok(())
+}
