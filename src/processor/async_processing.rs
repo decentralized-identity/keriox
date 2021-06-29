26b5e636ef1288022a8079b23ebbaa7e6e296d99
Ivan Temchenko <35359595i@gmail.com>
2021-06-29 16:02:06 +0200

    feat(async) process with reader only

--- src/processor/async_processing.rs
+++ src/processor/async_processing.rs
@@ -3,38 +3,34 @@ use std::{
     pin::Pin,
 };
 use async_std::{
-    io::{BufRead, BufReader, Read, Write},
+    io::{BufRead, BufReader, Read},
     task::{Context, Poll},
 };
 use crate::{
     event_message::parse::message,
     state::IdentifierState,
-    error::Error,
 };
 use pin_project_lite::pin_project;
 
 pub type Result<T> = std::result::Result<T, String>;
 
-pub async fn process<R, W>(reader: &mut R, writer: &mut W) -> Result<()>
+pub async fn process<R>(reader: &mut R) -> Result<()>
 where
-    R: Read + Unpin + ?Sized,
-    W: Write + Unpin + ?Sized
+    R: Read + Unpin + ?Sized
 {
         pin_project! {
-            struct Processor<R, W> {
+            struct Processor<R> {
                 #[pin]
                 reader: R,
                 #[pin]
-                writer: W,
-                #[pin]
                 state: IdentifierState,
+                processed: usize
             }
         }
 
-        impl<R, W> Future for Processor<R, W>
+        impl<R> Future for Processor<R>
         where
-            R: BufRead,
-            W: Write + Unpin
+            R: BufRead
         {
             type Output = Result<()>;
 
@@ -44,18 +40,17 @@ where
                     let buffer = futures_core::ready!(this.reader.as_mut().poll_fill_buf(cx)).map_err(|e| e.to_string())?;
                     let amt = buffer.len();
                     if buffer.is_empty() {
-                        futures_core::ready!(this.writer.as_mut().poll_flush(cx)).map_err(|e| e.to_string())?;
-                        return Poll::Ready(Ok(()));
+                        if *this.processed == 0usize {
+                            return Poll::Ready(Err(String::from("empty message")));
+                        } else {
+                            return Poll::Ready(Ok(()));
+                        }
                     }
 
+                    *this.processed += 1;
                     let message = message(buffer).map_err(|e| e.to_string())?;
                     let new_state = this.state.clone().apply(&message.1.event).map_err(|e| e.to_string())?;
                     *this.state.as_mut() = new_state;
-                    // TODO: what should be returned on successful message processing?
-                    let i = futures_core::ready!(this.writer.as_mut().poll_write(cx, &1i32.to_ne_bytes())).map_err(|e| e.to_string())?;
-                    if i == 0 {
-                        return Poll::Ready(Err(Box::new(Error::ZeroSendError).to_string()));
-                    }
                     this.reader.as_mut().consume(amt);
                 }
             }
@@ -63,8 +58,8 @@ where
 
         let processor = Processor {
             reader: BufReader::new(reader),
-            writer,
             state: IdentifierState::default(),
+            processed: 0
         };
         processor.await
 }
