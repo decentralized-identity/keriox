5037a7d47e66e30b65221c4151353c58262ec59b
Ivan Temchenko <35359595i@gmail.com>
2021-07-01 18:07:04 +0200

    feat(async) single message processing

--- src/processor/async_processing.rs
+++ src/processor/async_processing.rs
@@ -1,65 +1,51 @@
 use std::{
     future::Future,
     pin::Pin,
+    path::Path,
 };
-use async_std::{
-    io::{BufRead, BufReader, Read},
-    task::{Context, Poll},
-};
-use crate::{
-    event_message::parse::message,
-    state::IdentifierState,
-};
+use async_std::{io::{BufRead, BufReader, Read}, task::{Context, Poll}};
+use crate::{database::sled::SledEventDatabase, keri::Keri, prefix::IdentifierPrefix, signer::CryptoBox};
 use pin_project_lite::pin_project;
 
 pub type Result<T> = std::result::Result<T, String>;
 
-pub async fn process<R>(reader: &mut R) -> Result<()>
+pub async fn process<'a, R>(reader: &mut R) -> Result<Vec<u8>>
 where
     R: Read + Unpin + ?Sized
 {
         pin_project! {
-            struct Processor<R> {
+            struct Processor<'a, R> {
                 #[pin]
                 reader: R,
                 #[pin]
-                state: IdentifierState,
-                processed: usize
+                keri: Keri<'a, CryptoBox>,
             }
         }
 
-        impl<R> Future for Processor<R>
+        impl<'a, R> Future for Processor<'a, R>
         where
             R: BufRead
         {
-            type Output = Result<()>;
+            type Output = Result<Vec<u8>>;
 
             fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
                 let mut this = self.project();
-                loop {
-                    let buffer = futures_core::ready!(this.reader.as_mut().poll_fill_buf(cx)).map_err(|e| e.to_string())?;
-                    let amt = buffer.len();
-                    if buffer.is_empty() {
-                        if *this.processed == 0usize {
-                            return Poll::Ready(Err(String::from("empty message")));
-                        } else {
-                            return Poll::Ready(Ok(()));
-                        }
-                    }
-
-                    *this.processed += 1;
-                    let message = message(buffer).map_err(|e| e.to_string())?;
-                    let new_state = this.state.clone().apply(&message.1.event).map_err(|e| e.to_string())?;
-                    *this.state.as_mut() = new_state;
-                    this.reader.as_mut().consume(amt);
+                let buffer = futures_core::ready!(this.reader.as_mut().poll_fill_buf(cx)).map_err(|e| e.to_string())?;
+                if buffer.is_empty() {
+                    return Poll::Ready(Err(String::from("empty message")));
                 }
+                let response = this.keri.as_mut().respond(buffer).map_err(|e| e.to_string())?;
+                Poll::Ready(Ok(response))
             }
         }
 
+        let path = Path::new("./keri.db");
+        let db = SledEventDatabase::new(path).map_err(|e| e.to_string())?;
+        let cb = CryptoBox::new().map_err(|e| e.to_string())?;
+        let keri = Keri::new(&db, cb, IdentifierPrefix::default()).map_err(|e| e.to_string())?;
         let processor = Processor {
             reader: BufReader::new(reader),
-            state: IdentifierState::default(),
-            processed: 0
+            keri,
         };
-        processor.await
+            processor.await
 }
