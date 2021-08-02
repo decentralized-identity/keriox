aad9db305f2ce5d42ed7d6b4ebc8c9bbf357872d
Edyta Pawlak <edyta.pawlakk@gmail.com>
2021-08-02 19:56:00 +0200

    refactor(signer): refactor CryptoBox struct

--- src/signer/mod.rs
+++ src/signer/mod.rs
@@ -12,7 +12,7 @@ pub struct CryptoBox {
     signer: Signer,
     next_priv_key: Key,
     pub next_pub_key: Key,
-    seeds: Vec<String>,
+    seeds: Box<dyn Iterator<Item = SeedPrefix>>,
 }
 
 impl KeyManager for CryptoBox {
@@ -29,17 +29,10 @@ impl KeyManager for CryptoBox {
     }
 
     fn rotate(&mut self) -> Result<(), Error> {
-        let (next_pub_key, next_priv_key) =
-            if let Some((next_secret, next_seeds)) = self.seeds.split_first() {
-                let next_secret: SeedPrefix = next_secret.parse()?;
-                self.seeds = next_seeds.to_vec();
-                next_secret.derive_key_pair()?
-            } else {
-                let kp = Keypair::generate(&mut OsRng {});
-                let vk = Key::new(kp.public.as_bytes().to_vec());
-                let sk = Key::new(kp.secret.as_bytes().to_vec());
-                (vk, sk)
-            };
+        let (next_pub_key, next_priv_key) = {
+            let nxt_key_seed = self.seeds.next();
+            derive_key_pair(nxt_key_seed)?
+        };
 
         let new_signer = Signer {
             priv_key: self.next_priv_key.clone(),
@@ -55,60 +48,27 @@ impl KeyManager for CryptoBox {
 //#[cfg(feature = "demo")]
 impl CryptoBox {
     pub fn new() -> Result<Self, Error> {
-        let signer = Signer::new()?;
-        let kp = Keypair::generate(&mut OsRng {});
-        let (next_pub_key, next_priv_key) = (
-            Key::new(kp.public.as_bytes().to_vec()),
-            Key::new(kp.secret.as_bytes().to_vec()),
-        );
+        let signer = Signer::new();
+        let (next_pub_key, next_priv_key) = derive_key_pair(None)?;
         Ok(CryptoBox {
             signer,
             next_pub_key,
             next_priv_key,
-            seeds: vec![],
+            seeds: Box::new(std::iter::empty()),
         })
     }
 
-    pub fn derive_from_seed(seeds: &[&str]) -> Result<Self, Error> {
-        let (pub_key, priv_key) = match seeds.get(0) {
-            Some(secret) => {
-                let secret = SecretKey::from_bytes(secret.as_bytes()).map_err(|_| {
-                    Error::SemanticError("failed to convert provided seet to SecretKey".into())
-                })?;
-                let public = PublicKey::from(&secret);
-                (public, secret)
-            }
-            None => ed_new_public_private(),
-        };
+    pub fn derive_from_seed(seeds: Vec<SeedPrefix>) -> Result<Self, Error> {
+        let mut seeds = seeds.into_iter();
+        let (pub_key, priv_key) = derive_key_pair(seeds.next())?;
 
-        let (next_pub_key, next_priv_key) = match seeds.get(1) {
-            Some(secret) => {
-                let seed: SeedPrefix = secret.parse()?;
-                seed.derive_key_pair()?
-            }
-            None => {
-                let (vk, sk) = ed_new_public_private();
-                let vk = Key::new(vk.to_bytes().to_vec());
-                let sk = Key::new(sk.to_bytes().to_vec());
-                (vk, sk)
-            }
-        };
-
-        let seeds = seeds
-            .get(2..)
-            .unwrap_or(&vec![])
-            .iter()
-            .map(|s| s.to_string())
-            .collect();
+        let (next_pub_key, next_priv_key) = derive_key_pair(seeds.next())?;
 
         Ok(CryptoBox {
-            signer: Signer {
-                pub_key: Key::new(pub_key.to_bytes().to_vec()),
-                priv_key: Key::new(priv_key.to_bytes().to_vec()),
-            },
+            signer: Signer { pub_key, priv_key },
             next_pub_key,
             next_priv_key,
-            seeds,
+            seeds: Box::new(seeds),
         })
     }
 }
@@ -119,12 +79,12 @@ struct Signer {
 }
 
 impl Signer {
-    pub fn new() -> Result<Self, Error> {
-        let ed = Keypair::generate(&mut OsRng);
-        let pub_key = Key::new(ed.public.to_bytes().to_vec());
-        let priv_key = Key::new(ed.secret.to_bytes().to_vec());
+    pub fn new() -> Self {
+        let (pk, sk) = ed_new_public_private();
+        let pub_key = Key::new(pk.to_bytes().to_vec());
+        let priv_key = Key::new(sk.to_bytes().to_vec());
 
-        Ok(Signer { pub_key, priv_key })
+        Signer { pub_key, priv_key }
     }
 
     pub fn sign(&self, msg: &Vec<u8>) -> Result<Vec<u8>, Error> {
@@ -135,4 +95,68 @@ impl Signer {
 fn ed_new_public_private() -> (PublicKey, SecretKey) {
     let kp = Keypair::generate(&mut OsRng {});
     (kp.public, kp.secret)
+}
+
+fn derive_key_pair(seed: Option<SeedPrefix>) -> Result<(Key, Key), Error> {
+    match seed {
+        Some(secret) => secret.derive_key_pair(),
+        None => {
+            let (vk, sk) = ed_new_public_private();
+            let vk = Key::new(vk.to_bytes().to_vec());
+            let sk = Key::new(sk.to_bytes().to_vec());
+            Ok((vk, sk))
+        }
+    }
+}
+
+#[test]
+fn test_derive_keypairs_from_seed() -> Result<(), Error> {
+    use base64;
+    // taken from KERIPY: tests/core/test_eventing.py#1512
+    let seeds = vec![
+        "ArwXoACJgOleVZ2PY7kXn7rA0II0mHYDhc6WrBH8fDAc",
+        "A6zz7M08-HQSFq92sJ8KJOT2cZ47x7pXFQLPB0pckB3Q",
+        "AcwFTk-wgk3ZT2buPRIbK-zxgPx-TKbaegQvPEivN90Y",
+        "Alntkt3u6dDgiQxTATr01dy8M72uuaZEf9eTdM-70Gk8",
+    ];
+
+    let expected_pubkeys = vec![
+        "SuhyBcPZEZLK-fcw5tzHn2N46wRCG_ZOoeKtWTOunRA=",
+        "VcuJOOJF1IE8svqEtrSuyQjGTd2HhfAkt9y2QkUtFJI=",
+        "T1iAhBWCkvChxNWsby2J0pJyxBIxbAtbLA0Ljx-Grh8=",
+        "KPE5eeJRzkRTMOoRGVd2m18o8fLqM2j9kaxLhV3x8AQ=",
+    ];
+
+    let mut cryptobox =
+        CryptoBox::derive_from_seed(seeds.iter().map(|seed| seed.parse().unwrap()).collect())?;
+    assert_eq!(
+        base64::encode_config(cryptobox.public_key().key(), base64::URL_SAFE),
+        expected_pubkeys[0]
+    );
+    assert_eq!(
+        base64::encode_config(cryptobox.next_public_key().key(), base64::URL_SAFE),
+        expected_pubkeys[1]
+    );
+
+    cryptobox.rotate()?;
+    assert_eq!(
+        base64::encode_config(cryptobox.public_key().key(), base64::URL_SAFE),
+        expected_pubkeys[1]
+    );
+    assert_eq!(
+        base64::encode_config(cryptobox.next_public_key().key(), base64::URL_SAFE),
+        expected_pubkeys[2]
+    );
+
+    cryptobox.rotate()?;
+    assert_eq!(
+        base64::encode_config(cryptobox.public_key().key(), base64::URL_SAFE),
+        expected_pubkeys[2]
+    );
+    assert_eq!(
+        base64::encode_config(cryptobox.next_public_key().key(), base64::URL_SAFE),
+        expected_pubkeys[3]
+    );
+
+    Ok(())
 }
