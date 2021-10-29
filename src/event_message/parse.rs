9fc6c04e7fecfe0b4ad6bfcd67d209e750efb4ac
Edyta Pawlak <edyta.pawlakk@gmail.com>
2021-10-29 20:12:56 +0200

    test(parse): add attachment parsing test

--- src/event_message/parse.rs
+++ src/event_message/parse.rs
@@ -138,23 +138,6 @@ pub(crate) fn version<'a>(data: &'a [u8]) -> nom::IResult<&[u8], SerializationIn
     alt((json_version, cbor_version, mgpk_version))(data).map(|d| (d.0, d.1))
 }
 
-/// extracts the count from the sig count code
-// FIXME: is this working for all types of sigs?
-pub(crate) fn sig_count(s: &[u8]) -> nom::IResult<&[u8], u16> {
-    let (rest, t) = tuple((
-        map_parser(
-            nom::bytes::complete::take(2u8),
-            tuple((
-                nom::bytes::complete::tag("-"),
-                nom::bytes::complete::tag("A"),
-            )),
-        ),
-        b64_count,
-    ))(s)?;
-
-    Ok((rest, t.1))
-}
-
 /// returns attached source seals
 pub(crate) fn source_seal(s: &[u8]) -> nom::IResult<&[u8], Vec<SourceSeal>> {
     let (rest, sc) = b64_count(s)?;
@@ -204,22 +187,6 @@ pub(crate) fn b64_count(s: &[u8]) -> nom::IResult<&[u8], u16> {
 pub fn signatures(s: &[u8]) -> nom::IResult<&[u8], Vec<AttachedSignaturePrefix>> {
     let (rest, sc) = b64_count(s)?;
     count(attached_signature, sc as usize)(rest)
-}
-
-/// called on an attached signature stream starting with a sig count (with payload type)
-fn controller_signatures(s: &[u8]) -> nom::IResult<&[u8], Vec<AttachedSignaturePrefix>> {
-    let (rest, t) = tuple((
-        map_parser(
-            nom::bytes::complete::take(2u8),
-            tuple((
-                nom::bytes::complete::tag("-"),
-                nom::bytes::complete::tag("A"),
-            )),
-        ),
-        signatures,
-    ))(s)?;
-
-    Ok((rest, t.1))
 }
 
 fn couplets(s: &[u8]) -> nom::IResult<&[u8], Vec<(BasicPrefix, SelfSigningPrefix)>> {
@@ -376,37 +343,40 @@ pub fn signed_event_stream_validate(s: &[u8]) -> nom::IResult<&[u8], IdentifierS
 }
 
 #[test]
-fn test_sigs() {
-    use crate::{derivation::self_signing::SelfSigning, prefix::AttachedSignaturePrefix};
-    assert_eq!(sig_count("-AAA".as_bytes()), Ok(("".as_bytes(), 0u16)));
-    assert_eq!(sig_count("-ABA".as_bytes()), Ok(("".as_bytes(), 64u16)));
+fn test_b64_count() {
+    assert_eq!(b64_count("AA".as_bytes()), Ok(("".as_bytes(), 0u16)));
+    assert_eq!(b64_count("BA".as_bytes()), Ok(("".as_bytes(), 64u16)));
     assert_eq!(
-        sig_count("-AABextra data and stuff".as_bytes(),),
+        b64_count("ABextra data and stuff".as_bytes(),),
         Ok(("extra data and stuff".as_bytes(), 1u16))
     );
+}
+
+#[test]
+fn test_sigs() {
+    use crate::{derivation::self_signing::SelfSigning, prefix::AttachedSignaturePrefix};
 
     assert_eq!(
-        controller_signatures("-AABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA".as_bytes()),
-        Ok(("".as_bytes(), vec![AttachedSignaturePrefix::new(SelfSigning::Ed25519Sha512, vec![0u8; 64], 0)]))
+        attachment("-AABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA".as_bytes()),
+        Ok(("".as_bytes(), Attachment::AttachedSignatures(vec![AttachedSignaturePrefix::new(SelfSigning::Ed25519Sha512, vec![0u8; 64], 0)])))
     );
 
-    assert!(controller_signatures("-AABAA0Q7bqPvenjWXo_YIikMBKOg-pghLKwBi1Plm0PEqdv67L1_c6dq9bll7OFnoLp0a74Nw1cBGdjIPcu-yAllHAw".as_bytes()).is_ok());
-    // -AABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+    assert!(attachment("-AABAA0Q7bqPvenjWXo_YIikMBKOg-pghLKwBi1Plm0PEqdv67L1_c6dq9bll7OFnoLp0a74Nw1cBGdjIPcu-yAllHAw".as_bytes()).is_ok());
 
     assert_eq!(
-        controller_signatures("-AACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA0AACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAextra data".as_bytes()),
-        Ok(("extra data".as_bytes(), vec![
+        attachment("-AACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA0AACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAextra data".as_bytes()),
+        Ok(("extra data".as_bytes(), Attachment::AttachedSignatures(vec![
             AttachedSignaturePrefix::new(SelfSigning::Ed25519Sha512, vec![0u8; 64], 0),
             AttachedSignaturePrefix::new(SelfSigning::Ed448, vec![0u8; 114], 2)
-        ]))
+        ])))
     );
 
     assert_eq!(
-        controller_signatures("-AACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA0AACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA".as_bytes()),
-        Ok(("".as_bytes(), vec![
+        attachment("-AACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA0AACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA".as_bytes()),
+        Ok(("".as_bytes(), Attachment::AttachedSignatures(vec![
             AttachedSignaturePrefix::new(SelfSigning::Ed25519Sha512, vec![0u8; 64], 0),
             AttachedSignaturePrefix::new(SelfSigning::Ed448, vec![0u8; 114], 2)
-        ]))
+        ])))
     )
 }
 
@@ -455,31 +425,49 @@ fn test_event() {
 
 #[test]
 fn test_attachement() {
-    use crate::prefix::Prefix;
-
     let attached_str = "-GAC0AAAAAAAAAAAAAAAAAAAAAAQE3fUycq1G-P1K1pL2OhvY6ZU-9otSa3hXiCcrxuhjyII0AAAAAAAAAAAAAAAAAAAAAAQE3fUycq1G-P1K1pL2OhvY6ZU-9otSa3hXiCcrxuhjyII";
-    let (_rest, attached_sn_dig): (_, Attachment) = attachment(attached_str.as_bytes()).unwrap();
+    let (_rest, attached_sn_dig) = attachment(attached_str.as_bytes()).unwrap();
     assert_eq!(
-        attached_str,
-        String::from_utf8(attached_sn_dig.serialize().unwrap()).unwrap()
+        attached_sn_dig,
+        Attachment::SealSourceCouplets(vec![
+            SourceSeal {
+                sn: 1,
+                digest: "E3fUycq1G-P1K1pL2OhvY6ZU-9otSa3hXiCcrxuhjyII"
+                    .parse()
+                    .unwrap()
+            },
+            SourceSeal {
+                sn: 1,
+                digest: "E3fUycq1G-P1K1pL2OhvY6ZU-9otSa3hXiCcrxuhjyII"
+                    .parse()
+                    .unwrap()
+            }
+        ])
     );
-    match attached_sn_dig {
-        Attachment::SealSourceCouplets(sources) => {
-            let s1 = sources[0].clone();
-            let s2 = sources[1].clone();
-            assert_eq!(s1.sn, 1);
-            assert_eq!(
-                s1.digest.to_str(),
-                "E3fUycq1G-P1K1pL2OhvY6ZU-9otSa3hXiCcrxuhjyII"
-            );
-            assert_eq!(s2.sn, 1);
-            assert_eq!(
-                s2.digest.to_str(),
-                "E3fUycq1G-P1K1pL2OhvY6ZU-9otSa3hXiCcrxuhjyII"
-            );
-        }
-        _ => {}
-    };
+
+    let attached_str = "-FABED9EB3sA5u2vCPOEmX3d7bEyHiSh7Xi8fjew2KMl3FQM0AAAAAAAAAAAAAAAAAAAAAAAEeGqW24EnxUgO_wfuFo6GR_vii-RNv5iGo8ibUrhe6Z0";
+    let (_rest, seal) = attachment(attached_str.as_bytes()).unwrap();
+    assert_eq!(
+        seal,
+        Attachment::AttachedEventSeal(vec![EventSeal {
+            prefix: "ED9EB3sA5u2vCPOEmX3d7bEyHiSh7Xi8fjew2KMl3FQM"
+                .parse()
+                .unwrap(),
+            sn: 0,
+            event_digest: "EeGqW24EnxUgO_wfuFo6GR_vii-RNv5iGo8ibUrhe6Z0"
+                .parse()
+                .unwrap()
+        },])
+    );
+
+    let attached_str = "-CABBed2Tpxc8KeCEWoq3_RKKRjU_3P-chSser9J4eAtAK6I0B8npsG58rX1ex73gaGe-jvRnw58RQGsDLzoSXaGn-kHRRNu6Kb44zXDtMnx-_8CjnHqskvDbz6pbEbed3JTOnCQ";
+    let (_rest, seal) = attachment(attached_str.as_bytes()).unwrap();
+    assert_eq!(seal, Attachment::ReceiptCouplets(
+        vec![
+            ("Bed2Tpxc8KeCEWoq3_RKKRjU_3P-chSser9J4eAtAK6I".parse().unwrap(), "0B8npsG58rX1ex73gaGe-jvRnw58RQGsDLzoSXaGn-kHRRNu6Kb44zXDtMnx-_8CjnHqskvDbz6pbEbed3JTOnCQ".parse().unwrap())
+            ]
+        )
+    );
 }
 
 #[test]
